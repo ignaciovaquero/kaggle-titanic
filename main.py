0839cbcec40fc716cfc31a0a61ebabc7fc888a43
Ignacio Vaquero Guisasola <ignacio.vaquero.guisasola@ing.com>
2020-05-25 08:07:08 +0200

    Added Column Transformer for transforming data values

--- main.py
+++ main.py
@@ -3,24 +3,32 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-import matplotlib.pyplot as plt
-
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == "__main__":
     data = pd.read_csv('data/train.csv')
 
     labels = data[['Name','Survived']]
     # Let's try next without dropping 'Cabin'
-    data = data.drop(columns=['Ticket', 'Cabin', 'Name','Survived'])
+    data = data.drop(columns=['PassengerId', 'Ticket',
+                              'Cabin', 'Name', 'Survived'])
     data = data.dropna(subset=['Embarked'])
-    data_num = data.drop(columns=['Embarked'])
-    data_cat_1h = data['Embarked']
-    imputer = SimpleImputer(strategy='median')
-    ordinal_enc = OrdinalEncoder()
-    data_cat = data[['Sex']]
-    data['Sex'] = ordinal_enc.fit_transform(data[['Sex']]).astype('int')
-    print(data.info())
-    print(labels.head())
-    # data_num = data.drop(columns=['Name'])
-    # data['Age'].fillna(data['Age'].median(), inplace=True)
-    # print(data['Ticket'].unique())
+    data_num = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+    data_cat = ['Sex']
+    data_1h = ['Embarked']
+    column_transformer = ColumnTransformer([
+        ("num", SimpleImputer(strategy='median'), data_num),
+        ("cat", OrdinalEncoder(), data_cat),
+        ("1h", OneHotEncoder(sparse=False), data_1h)
+        ])
+
+    pipeline = Pipeline([("cleaning", column_transformer), ("scaling", StandardScaler())])
+    array_processed = pipeline.fit_transform(data)
+    data_processed = pd.DataFrame(data=array_processed, columns=[
+        'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex', 'C', 'Q', 'S'
+        ])
+    # data_float = data_processed['Fare']
+    # data_processed = data_processed.drop(columns='Fare').astype('int')
+    # data_processed['Fare'] = data_float
+    print(data_processed.info())
+    print(labels.info())
