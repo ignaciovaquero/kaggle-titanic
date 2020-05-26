32663e49779d46e612686e772089d10eca7c5cbf
Ignacio Vaquero Guisasola <ignacio.vaquero.guisasola@ing.com>
2020-05-26 08:10:39 +0200

    Finished cleaning data

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 
-if __name__ == "__main__":
-    data = pd.read_csv('data/train.csv')
 
-    labels = data[['Name','Survived']]
+def transform_data(data):
+    '''
+    Clean and transform Titanic Data
+    '''
+
     # Let's try next without dropping 'Cabin'
     data = data.drop(columns=['PassengerId', 'Ticket',
                               'Cabin', 'Name', 'Survived'])
@@ -20,15 +22,29 @@ if __name__ == "__main__":
         ("num", SimpleImputer(strategy='median'), data_num),
         ("cat", OrdinalEncoder(), data_cat),
         ("1h", OneHotEncoder(sparse=False), data_1h)
-        ])
+    ])
 
-    pipeline = Pipeline([("cleaning", column_transformer), ("scaling", StandardScaler())])
-    array_processed = pipeline.fit_transform(data)
+    array_processed = column_transformer.fit_transform(data)
     data_processed = pd.DataFrame(data=array_processed, columns=[
         'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex', 'C', 'Q', 'S'
-        ])
-    # data_float = data_processed['Fare']
-    # data_processed = data_processed.drop(columns='Fare').astype('int')
-    # data_processed['Fare'] = data_float
-    print(data_processed.info())
+    ])
+    data_float = data_processed[['Fare', 'Age']]
+    data_processed = data_processed.drop(columns=['Fare', 'Age']).astype('int')
+    data_processed[['Fare', 'Age']] = data_float
+    scaler = StandardScaler()
+    data_processed[['Fare', 'Age']] = scaler.fit_transform(
+        data_processed[['Fare', 'Age']])
+    return data_processed
+
+
+def predict():
+    pass
+
+
+if __name__ == "__main__":
+    data = pd.read_csv('data/train.csv')
+
+    labels = data[['Name','Survived']]
+    data = transform_data(data)
+    print(data.head())
     print(labels.info())
